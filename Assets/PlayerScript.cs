3fe7acb892d4e58f7c5ed65010dac8406a7a00b0
Felipe Jiménez <felipe.zxkq@gmail.com>
2022-06-17 16:10:23 -0400

    First commit

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] float speed=3;
+    float h, v;
+    Vector3 moveDirection;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move(){
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        moveDirection.x = h;
+        moveDirection.y = v;
+        
+
+        transform.position += moveDirection * Time.deltaTime * speed;
+    }
+
+}
