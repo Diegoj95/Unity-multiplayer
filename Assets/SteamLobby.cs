0837488ea52673dc24c86ae84771ee2049d6c392
Diegoj95 <diegojimenez@gmail.com>
2022-06-26 22:42:36 -0400

    Lobby with Steam name and icon

--- Assets/SteamLobby.cs
+++ Assets/SteamLobby.cs
@@ -9,6 +9,7 @@ using Mirror;
 
 public class SteamLobby : MonoBehaviour
 {
+    public static SteamLobby Instance;
     //Callbacks
     protected Callback<LobbyCreated_t> LobbyCreated;
     protected Callback<GameLobbyJoinRequested_t> JoinRequest;
@@ -19,13 +20,13 @@ public class SteamLobby : MonoBehaviour
     private const string HostAddressKey = "HostAddress";
     private CustomNetworkManager manager;
     
-    //GameObject
-    public GameObject HostButton;
-    public Text LobbyNameText;
 
     private void Start()
     {
         if(!SteamManager.Initialized) {  return; }
+
+        if(Instance == null) { Instance = this; }
+
 
         manager = GetComponent<CustomNetworkManager>();
 
@@ -58,11 +59,7 @@ public class SteamLobby : MonoBehaviour
 
     private void OnLobbyEntered(LobbyEnter_t callback)
     {
-        //Everyone
-        HostButton.SetActive(false);
         CurrentLobbyID = callback.m_ulSteamIDLobby;
-        LobbyNameText.gameObject.SetActive(true);
-        LobbyNameText.text = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), "name");
 
         //Client
         if(NetworkServer.active) { return; }
